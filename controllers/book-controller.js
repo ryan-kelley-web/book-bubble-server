7283d11ddb63c1387ef97f0ed5e6a9fd8b65d20c
Sean Dwyer <spdwyer06@gmail.com>
2020-11-16 17:53:44 -0500

    Sean initial commit

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -3,6 +3,21 @@ const book = require('../db').import('../models/book');
 const validateSession = require('../middleware/validate-session');
 
 //Add books ---- (post) goes to BookCreate component (i.e. book/create)
+router.post('/create', (req, res) => {
+    console.log(req.user);
+    let bookModel = {
+        author: req.body.author,
+        title: req.body.title,
+        genre: req.body.genre,
+        numOfPages: req.body.numOfPages,
+        rating: req.body.rating,
+        owner: req.user.id
+    };
+
+    Book.create(bookModel)
+        .then(book => res.status(200).json(book))
+        .catch(err => res.status(500).json({error: err}));
+});
 
 //Get books already read ---- goes to BookTable component --- if you click “see more” then it goes to Book to display one book with all the deets, (i.e. /book/read) 
 
@@ -11,9 +26,26 @@ const validateSession = require('../middleware/validate-session');
 //Get books to read ---- goes to BookTable component --- if you click “see more” then it goes to Book to display one book with all the deets (i.e. /book/to-read)
 
 //Update book ---- goes to BookEdit component (i.e. /book/:id)
+router.put('/:id', (req, res) => {
+    const updatedBook = {
+        author: req.body.author,
+        title: req.body.title,
+        genre: req.body.genre,
+        numOfPages: req.body.numOfPages,
+        rating: req.body.rating
+    };
+
+    Book.update(updatedBook, {where: {id: req.params.bookId}})
+        .then(book => res.status(200).json(book))
+        .catch(err => res.status(500).json({error: err}));
+});
 
 //Delete book ----goes to BookEdit component (i.e. /book/:id)
-
+router.delete('/:id', (req, res) => {
+    Book.destroy({where: {id: req.params.bookId}})
+        .then(() => res.status(200).json({message: 'Book has been deleted'}))
+        .catch(err => res.status(500).json({error: err}));
+});
 
 
 module.exports = router; 
