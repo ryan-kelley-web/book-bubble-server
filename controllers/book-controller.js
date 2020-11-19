b76d9324bc3048966fa92b7d7ba285222130dd89
lyndsaycrespo <lyndsaycrespo@gmail.com>
2020-11-19 15:52:33 -0500

    fixed stuff

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,99 +1,119 @@
-const router = require('express').Router();
-const book = require('../db').import('../models/book');
-const { restart } = require('nodemon');
-const validateSession = require('../middleware/validate-session');
+const router = require("express").Router();
+const book = require("../db").import("../models/book");
+const { restart } = require("nodemon");
+const validateSession = require("../middleware/validate-session");
+const { Op } = require("sequelize");
 
 //Add books ---- (post) goes to BookCreate component (i.e. book/create)
-router.post('/create', (req, res) => {
-    console.log(req.user);
-    let bookModel = {
-        author: req.body.author,
-        title: req.body.title,
-        genre: req.body.genre,
-        total_pages: req.body.total_pages,
-        rating: req.body.rating,
-        description: req.body.description,
-        year_published: req.body.year_published,
-        read_status: req.body.read_status,
-        owner_id: req.user.id
-    };
+router.post("/create", (req, res) => {
+  //console.log(req.user);
+  let bookModel = {
+    author: req.body.author,
+    title: req.body.title,
+    genre: req.body.genre,
+    total_pages: req.body.total_pages,
+    rating: req.body.rating,
+    description: req.body.description,
+    year_published: req.body.year_published,
+    read_status: req.body.read_status,
+    owner_id: req.user.id,
+  };
 
-    book.create(bookModel)
-        .then(book => console.log('Book Create:', book))
-        .then(book => res.status(200).json(book))
-        .catch(err => res.status(500).json({error: err}));
+  book
+    .create(bookModel)
+    //.then((book) => console.log("Book Create:", book))
+    .then((book) => res.status(200).json(book))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
-//Get books already read ---- if you click “see more” then it goes to Book to display one book with all the deets, (i.e. /book/read) 
-router.get('/read', validateSession, (req, res) => { 
-    let userid = req.user.id //make sure owner_id = req.user.id (check user model & rest of controller)
-    book.findAll({
-        where: {owner_id: userid, read_status: 'read'}
-    }) 
-        .then(books => res.status(200).json(books)) 
-        .catch(err => res.status(500).json({error: err})) 
+//Get books already read ---- if you click “see more” then it goes to Book to display one book with all the deets, (i.e. /book/read)
+router.get("/read", validateSession, (req, res) => {
+  let userid = req.user.id; //make sure owner_id = req.user.id (check user model & rest of controller)
+  book
+    .findAll({
+      where: { owner_id: userid, read_status: "read" },
+    })
+    .then((books) => res.status(200).json(books))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 //Get books currently reading ---- if you click “see more” then it goes to Book to display one book with all the deets (i.e. /book/reading)
-router.get('/reading', validateSession, (req, res) => { 
-    let userid = req.user.id //make sure owner_id = req.user.id (check user model & rest of controller)
-    book.findAll({
-        where: {owner_id: userid, read_status: 'reading'}
-    }) 
-        .then(books => res.status(200).json(books)) 
-        .catch(err => res.status(500).json({error: err})) 
+router.get("/reading", validateSession, (req, res) => {
+  let userid = req.user.id; //make sure owner_id = req.user.id (check user model & rest of controller)
+  book
+    .findAll({
+      where: { owner_id: userid, read_status: "reading" },
+    })
+    .then((books) => res.status(200).json(books))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 //Get books to read --- if you click “see more” then it goes to Book to display one book with all the deets (i.e. /book/to-read)
-router.get('/to-read', validateSession, (req, res) => { 
-    let userid = req.user.id //make sure owner_id = req.user.id (check user model & rest of controller)
-    book.findAll({
-        where: {owner_id: userid, read_status: 'to-read'}
-    }) 
-        .then(books => res.status(200).json(books)) 
-        .catch(err => res.status(500).json({error: err})) 
+router.get("/to-read", validateSession, (req, res) => {
+  let userid = req.user.id; //make sure owner_id = req.user.id (check user model & rest of controller)
+  book
+    .findAll({
+      where: { owner_id: userid, read_status: "to-read" },
+    })
+    .then((books) => res.status(200).json(books))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 //Update book ---- goes to BookEdit component (i.e. /book/:id)
 // May need to change route to /:bookId
-router.put('/edit/:bookId', validateSession, function(req, res) {
-    const updateBookInfo = {
-        author: req.body.author,
-        title: req.body.title,
-        genre: req.body.genre,
-        total_pages: req.body.total_pages,
-        rating: req.body.rating,
-        description: req.body.description,
-        year_published: req.body.year_published,
-        read_status: req.body.read_status,
-    };
+router.put("/edit/:bookId", validateSession, function (req, res) {
+  const updateBookInfo = {
+    author: req.body.author,
+    title: req.body.title,
+    genre: req.body.genre,
+    total_pages: req.body.total_pages,
+    rating: req.body.rating,
+    description: req.body.description,
+    year_published: req.body.year_published,
+    read_status: req.body.read_status,
+  };
 
-    const query = { where: { owner_id: req.body.owner_id } }; //req.params.owner_id?
+  const query = { where: { owner_id: req.body.owner_id } }; //req.params.owner_id?
 
-    book.update(updateBookInfo, query)
-    .then((book) => res.status(200).json({message: 'Your book has been edited successfully'})) // books plural?
-    .catch(err=> res.status(500).json({error: err}));
+  book
+    .update(updateBookInfo, query)
+    .then((book) =>
+      res
+        .status(200)
+        .json({ message: "Your book has been edited successfully" })
+    ) // books plural?
+    .catch((err) => res.status(500).json({ error: err }));
 });
-
 
 //Delete book ----goes to BookEdit component (i.e. /book/:id)
 // May need to change route to /:bookId
-router.delete('/:id', (req, res) => {
-    book.destroy({where: {id: req.params.id}})
-        .then(() => res.status(200).json({message: 'Book has been deleted'}))
-        .catch(err => res.status(500).json({error: err}));
+router.delete("/:id", (req, res) => {
+  book
+    .destroy({ where: { id: req.params.id } })
+    .then(() => res.status(200).json({ message: "Book has been deleted" }))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 //BookFinder/Get books by search
-router.get('/search/:query', validateSession, (req, res) => { 
-    const { query } = req.params; 
-    let userid = req.user.id;
-    book.findAll({ 
-        where: { owner_id: userid, title: { [Op.like]: `%${query}%`}, author: { [Op.like]: `%${query}%`} } //is this && or ||?
-    })
-    .then(books => res.status(200).json(books)) 
-    .catch(err => res.status(500).json({error: err})) 
+router.get("/search/:query", validateSession, (req, res) => {
+  const { query } = req.params;
+  let userid = req.user.id;
+  console.log(userid, query);
+  book
+    .findAll(
+        { where: {
+        [Op.or]: [
+            {[Op.and]: [
+                { title: { [Op.substring]: query} }, 
+                { owner_id: userid }
+            ]},
+            {[Op.and]: [
+                { author: { [Op.substring]: query} }, 
+                { owner_id: userid }
+            ]}
+        ]}})
+    .then((books) => res.status(200).json(books))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
-module.exports = router; 
-
+module.exports = router;
